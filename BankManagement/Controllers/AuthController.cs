1832c35d3376c2e66f9421f08595ca107c679d59
mfrc <mfrc@MRC>
2024-11-21 22:06:37 -0300

    Configure JWT Auth

--- BankManagement/Controllers/AuthController.cs
+++ BankManagement/Controllers/AuthController.cs
@@ -1,6 +1,45 @@
-﻿namespace BankManagement.Controllers;
+﻿using Core.Interfaces.Services.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-public class AuthController
+namespace BankManagement.Controllers;
+
+public class AuthController(IJwtProvider jwtProvider) : BaseApiController
 {
+    private readonly IJwtProvider _jwtProvider = jwtProvider;
+
+    [HttpGet("Generate-Token")]
+    [AllowAnonymous]
+    public IActionResult GenerateToken([FromQuery] string id, string name, [FromQuery] IEnumerable<string> roles)
+    {
+        return Ok(_jwtProvider.GenerateToken(id, name, roles));
+    }
     
+    [HttpGet("Protected-Endpoint")]
+    [Authorize]
+    public IActionResult ProtectedEndpoint()
+    {
+        return Ok("Esto es un endpoint protegido");
+    }
+    
+    [HttpGet("Protected-Endpoint-Seguridad")]
+    [Authorize(Roles = "Seguridad")]
+    public IActionResult ProtectedSecurityRol()
+    {
+        return Ok("Acceso solo a miembros con rol de seguridad");
+    }
+    
+    [HttpGet("Protected-Endpoint-Admin")]
+    [Authorize(Roles = "Admin")]
+    public IActionResult ProtectedAdminRol()
+    {
+        return Ok("Acceso solo a miembros con rol de Admin");
+    }
+    
+    [HttpGet("Protected-Endpoint-Ambos")]
+    [Authorize(Roles = "Admin,Seguridad")]
+    public IActionResult ProtectedBothRol()
+    {
+        return Ok("Acceso a miebros con roles de Seguridad y Admin");
+    }
 }
--- Core/JWT/JwtOptions.cs
+++ Core/JWT/JwtOptions.cs
@@ -2,5 +2,8 @@
 
 public class JwtOptions
 {
-    
+    public static string SecretKey { get; set; } = string.Empty;
+    public static string Audience { get; set; } = string.Empty;
+    public static string Issuer { get; set; } = string.Empty;
+    public static int ExpirationTime { get; set; }
 }
